43e3660ee152a6da923c88e8d7846aa088d989f2
Anwesh43 <anweshthecool0@gmail.com>
2019-10-29 10:28:07 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlinebouncyarcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinebouncyarcview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlinebouncyarcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linebouncyarcview.LineBouncyArcView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineBouncyArcView.create(this)
     }
 }
