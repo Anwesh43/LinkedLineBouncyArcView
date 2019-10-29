e18e44b4a42fe641a40b941a1baab9f74c273480
Anwesh43 <anweshthecool0@gmail.com>
2019-10-29 10:21:35 +0530

    adding LBANode class

--- linebouncyarcview/src/main/java/com/anwesh/uiprojects/linebouncyarcview/LineBouncyArcView.kt
+++ linebouncyarcview/src/main/java/com/anwesh/uiprojects/linebouncyarcview/LineBouncyArcView.kt
@@ -117,4 +117,46 @@ class LineBouncyArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBANode(var i : Int, val state : State = State()) {
+
+        private var next : LBANode? = null
+        private var prev : LBANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LBANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBANode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBANode {
+            var curr : LBANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
