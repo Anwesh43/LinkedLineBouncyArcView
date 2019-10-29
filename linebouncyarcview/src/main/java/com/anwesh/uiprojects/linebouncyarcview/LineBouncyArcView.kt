950f46496d90ba133b4b5d880d4528e56b1bcb74
Anwesh43 <anweshthecool0@gmail.com>
2019-10-29 10:12:46 +0530

    adding LineBouncyArcView class

--- linebouncyarcview/src/main/java/com/anwesh/uiprojects/linebouncyarcview/LineBouncyArcView.kt
+++ linebouncyarcview/src/main/java/com/anwesh/uiprojects/linebouncyarcview/LineBouncyArcView.kt
@@ -50,3 +50,21 @@ fun Canvas.drawLBANode(i : Int, scale : Float, paint : Paint) {
     drawLineBouncyArc(scale, size, paint)
     restore()
 }
+
+class LineBouncyArcView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
